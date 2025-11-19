597ac3ecbf186e45104c06ffb93bb09186d9bbd2
Heng <sivmiosu@gmail.com>
2025-11-20 01:17:19 +0700

    ex1

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Custom buttons"),
-        ),
-        body: Center(
-          child: SizedBox(
-                width: 400,
-                height: 100,
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Center(
-                      child: Text("Not Selected"),
-                    )),
-              ),
+        appBar: AppBar(title: const Text("Custom buttons")),
+        body: Center(child: CustomBottons()),
+      ),
+    );
+  }
+}
+
+class CustomBottons extends StatefulWidget {
+  const CustomBottons({super.key});
+
+  @override
+  State<CustomBottons> createState() => CustomBottonsState();
+}
+
+class CustomBottonsState extends State<CustomBottons> {
+  bool _selected = false;
+  String get _label => _selected ? "Selected" : "Not Selected";
+  Color get _textColor => _selected ? Colors.white : Colors.black;
+  Color get _backGroundColor => _selected ? Colors.blue : Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: _backGroundColor),
+        onPressed: () => setState(() => _selected = !_selected),
+        child: Center(
+          child: Text(
+            _label,
+            style: TextStyle(color: _textColor, fontSize: 24),
+          ),
         ),
       ),
-    ));
+    );
+  }
+}
